4c47aed8baf7ed24826d5239cf524129bc1ffce4
Luke Frisken <l.frisken@gmail.com>
2022-10-03 18:40:55 +1300

    Work in progress replies via inreach web interface (still producing 500 errors)

---
+++ src/inreach/reply.rs
@@ -0,0 +1,227 @@
+use std::{borrow::Cow, collections::HashMap, convert::TryFrom};
+
+use eyre::Context;
+use reqwest::Response;
+use serde::Serialize;
+use uuid::Uuid;
+
+struct Referral {
+    ext_id: Uuid,
+    adr: String,
+}
+
+impl TryFrom<&url::Url> for Referral {
+    type Error = eyre::Error;
+
+    fn try_from(url: &url::Url) -> Result<Self, Self::Error> {
+        let map: HashMap<Cow<str>, Cow<str>> = url.query_pairs().collect();
+        let ext_id: Uuid = map
+            .get("extId")
+            .ok_or_else(|| eyre::eyre!("extId query parameter is missing"))?
+            .parse()
+            .wrap_err("unable to parse extId query parameter")?;
+
+        let adr: String = map
+            .get("adr")
+            .ok_or_else(|| eyre::eyre!("adr query parameter is missing"))
+            .and_then(|adr| {
+                let decoded = urlencoding::decode(adr).wrap_err("unable to decode adr")?;
+                Ok(decoded.into_owned())
+            })?;
+
+        Ok(Self { ext_id, adr })
+    }
+}
+
+#[derive(Serialize)]
+#[serde(rename = "CamelCase")]
+struct PostFormData<'a> {
+    reply_address: &'a str,
+    reply_message: &'a str,
+    message_id: &'a str,
+    guid: Uuid,
+}
+
+/// Extract message id from the GET response body
+fn extract_message_id(html: &str) -> eyre::Result<String> {
+    let document = scraper::Html::parse_document(&html);
+    let selector =
+        scraper::Selector::parse("#MessageId").expect("Unable to parse MessageId selector");
+    let element_ref = document
+        .select(&selector)
+        .next()
+        .ok_or_else(|| eyre::eyre!("Expected a #MessageId element to be present"))?;
+    let element = element_ref.value();
+    let message_id = element
+        .attr("value")
+        .ok_or_else(|| eyre::eyre!("#MessageId input is missing `value` attribute"))?;
+    Ok(message_id.to_string())
+}
+
+pub async fn reply(
+    client: &reqwest::Client,
+    referral_url: &url::Url,
+    message: &str,
+) -> eyre::Result<()> {
+    // let encoded_message = encode_message(message);
+
+    let get_response = client
+        .get(referral_url.clone())
+        .header(
+            "User-Agent",
+            "Mozilla/5.0 (X11; Linux x86_64; rv:105.0) Gecko/20100101 Firefox/105.0",
+        )
+        .send()
+        .await
+        .and_then(Response::error_for_status)
+        .wrap_err("Error while performing GET request")?;
+
+    let get_response_html: String = get_response
+        .text()
+        .await
+        .wrap_err("Unable to decode GET response body")?;
+    let message_id: String = extract_message_id(&get_response_html)?;
+
+    if message_id.is_empty() {
+        eyre::bail!("Invalid message id received from server");
+    }
+
+    let referral: Referral = referral_url
+        .try_into()
+        .wrap_err("Unable to parse referral url")?;
+
+    let post_body: String = serde_urlencoded::to_string(PostFormData {
+        reply_address: &referral.adr,
+        reply_message: message,
+        message_id: &message_id,
+        guid: referral.ext_id,
+    })
+    .wrap_err("Unable to serialize POST form data")?;
+
+    // println!("headers: {:?}", response.headers());
+
+    // let request_context = response
+    //     .headers()
+    //     .get("request-context")
+    //     .ok_or_else(|| eyre::eyre!("Expected request-context header to be present in response"))?
+    //     .to_str().wrap_err("invalid request-context header unable to parse to utf8 string")?;
+    //
+    // let (key, value) = request_context.split_once('=').ok_or_else(|| eyre::eyre!("unexpected request-context header format: {:?}", request_context))?;
+
+    let mut post_url = referral_url.clone();
+    post_url.set_path("TextMessage/TxtMsg");
+    post_url.set_query(None);
+
+    let origin = post_url.origin().unicode_serialization();
+    let host = post_url.host_str().ok_or_else(|| eyre::eyre!("Unable to parse host from post url"))?.to_string();
+    let content_length = post_body.len();
+
+    let post_response = client
+        .post(post_url)
+        .body(post_body)
+        .header("Accept", "*/*")
+        .header("Accept-Encoding", "gzip, deflate, br")
+        .header("Cache-Control", "no-cache")
+        .header("Connection", "keep-alive")
+        .header("Content-Length", content_length)
+        .header(
+            "Content-Type",
+            "application/x-www-form-urlencoded; charset=UTF-8",
+        )
+        .header("Referrer", referral_url.as_str())
+        .header("X-Requested-With", "XMLHttpRequest")
+        .header("Host", host)
+        .header("Origin", origin)
+        .header("Sec-Fetch-Dest", "empty")
+        .header("Sec-Fetch-Mode", "cors")
+        .header("Sec-Fetch-Site", "same-origin")
+        .send()
+        .await
+        .and_then(Response::error_for_status)
+        .wrap_err("Error while performing POST request")?;
+
+    println!("POST status: {:?}", post_response.status());
+    println!("POST response:\n{}", post_response.text().await?);
+
+    Ok(())
+}
+
+#[cfg(test)]
+pub mod test {
+    use std::convert::TryFrom;
+
+    use url::Url;
+    use wiremock::{MockServer, Mock, matchers, ResponseTemplate};
+
+    use super::{extract_message_id, Referral};
+    use super::reply;
+
+    const GET_RESPONSE_BODY: &'static str = r#"
+    <html>
+        <body>
+            <input data-val="true" data-val-number="The field MessageId must be a number." data-val-required="The MessageId field is required." id="MessageId" name="MessageId" type="hidden" value="66270435">
+        </body>
+    </html>
+    "#;
+
+    #[test]
+    fn test_extract_message_id() {
+        let message_id = extract_message_id(GET_RESPONSE_BODY).unwrap();
+        assert_eq!("66270435", message_id);
+    }
+
+    #[test]
+    fn test_parse_referral_url() {
+        let url: Url = "https://aus.explore.garmin.com/textmessage/txtmsg?extId=08daa4e6-8eda-25c1-000d-3aa730600000&adr=email.weather.service%40gmail.com".parse().unwrap();
+
+        let referral = Referral::try_from(&url).unwrap();
+        assert_eq!(
+            uuid::uuid!("08daa4e6-8eda-25c1-000d-3aa730600000"),
+            referral.ext_id
+        );
+        assert_eq!("email.weather.service@gmail.com", referral.adr);
+    }
+
+    #[tokio::test]
+    async fn test_reply() {
+        let mock_server = MockServer::start().await;
+
+        let mut referral_url: Url = mock_server.uri().parse().unwrap();
+        referral_url.set_path("textmessage/txtmsg");
+        referral_url.set_query(Some(
+            "extId=08daa4e6-8eda-25c1-000d-3aa730600000&adr=email.weather.service%40gmail.com",
+        ));
+
+        Mock::given(matchers::method("GET"))
+            .and(matchers::path("/textmessage/txtmsg?extId=08daa4e6-8eda-25c1-000d-3aa730600000&adr=email.weather.service%40gmail.com"))
+            .respond_with(ResponseTemplate::new(200)
+                .insert_header("content-type", "text/html; charset=utf-8")
+                .insert_header("content-encoding", "br")
+                .insert_header("date", "Mon, 03 Oct 2022 03:10:40 GMT")
+                .insert_header("server", "cloudflare")
+                .insert_header("access-control-expose-headers", "Request-Context")
+                .insert_header("cache-control", "private")
+                .insert_header("cf-ray", "75427c6cb8f3a835-SYD")
+                .set_body_string(GET_RESPONSE_BODY)
+            )
+            .mount(&mock_server)
+            .await;
+
+        let success_body: String =
+            serde_json::to_string(&serde_json::json!({"Success": true})).unwrap();
+
+
+        Mock::given(matchers::method("POST"))
+            .and(matchers::path("/TextMessage/TxtMsg"))
+            .respond_with(ResponseTemplate::new(200)
+                .insert_header("content-type", "application/json; charset=utf-8")
+                .insert_header("content-encoding", "br")
+                .set_body_string(success_body)
+            )
+            .mount(&mock_server)
+            .await;
+
+        let client = reqwest::Client::new();
+        reply(&client, &referral_url, "Unit Test message, from Luke").await.unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use email_weather;
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    let referral_url: url::Url = "https://aus.explore.garmin.com/textmessage/txtmsg?extId=08daa4e6-8eda-25c1-000d-3aa730600000&adr=email.weather.service%40gmail.com".parse().unwrap();
+
+    let client = reqwest::Client::new();
+
+    let message = "Test From Rust";
+
+    email_weather::inreach::reply::reply(&client, &referral_url, message).await?;
+    
+    Ok(())
 }
